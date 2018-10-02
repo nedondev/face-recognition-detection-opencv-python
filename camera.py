ae4d3be8ed2738e936609588039a76596a65f5e6
user <nedondev@gmail.com>
2018-10-02 14:04:20 +0700

    Add use camera and get picture

---
+++ camera.py
@@ -0,0 +1,27 @@
+import cv2
+video_capture = cv2.VideoCapture(1)
+
+while True:
+    if not video_capture.isOpened():
+        print('Unable to load camera.')
+        sleep(5)
+        pass
+
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # Display the resulting frame
+    #cv2.imshow('Video', frame)
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
---
+++ getPicCamera.py
@@ -0,0 +1,38 @@
+import cv2
+import sys
+import logging as log
+import datetime as dt
+from time import sleep
+
+video_capture = cv2.VideoCapture(1)
+picCount = 0
+
+while True:
+    if not video_capture.isOpened():
+        print('Unable to load camera.')
+        sleep(5)
+        pass
+
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+    
+    inputKey = cv2.waitKey(1)
+    if inputKey == ord('c'):
+        status = cv2.imwrite('/home/user/Project/faceRag/myProject/pic'+str(picCount)+'.jpg',frame)
+        print('/home/user/Project/faceRag/myProject/pic'+str(picCount)+'.jpg have been saved.')
+        picCount += 1
+        inputKey = -2
+    elif inputKey == ord('q'):
+        break
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
